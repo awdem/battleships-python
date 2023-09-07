bdb0631cbc120493838581693e1266a59fe8b070
awdem <wdemontmolling@gmail.com>
2023-09-07 13:06:27 +0100

    tests: fixes ship setup to test full loop

--- tests/test_user_interface_integration.py
+++ tests/test_user_interface_integration.py
@@ -6,34 +6,113 @@ from tests.terminal_interface_helper_mock import TerminalInterfaceHelperMock
 
 
 class TestUserInterface(unittest.TestCase):
-    @pytest.mark.skip(reason="test is currently broken by loop")
     def test_ship_setup_scenario(self):
         io = TerminalInterfaceHelperMock()
         interface = UserInterface(io, Game())
-        io.expect_print("Welcome to the game!")
-        io.expect_print("Set up your ships first.")
-        io.expect_print("You have these ships remaining: 2, 3, 3, 4, 5")
-        io.expect_print("Which do you wish to place?")
-        io.provide("2")
-        io.expect_print("Vertical or horizontal? [vh]")
-        io.provide("v")
-        io.expect_print("Which row?")
-        io.provide("3")
-        io.expect_print("Which column?")
-        io.provide("2")
-        io.expect_print("OK.")
-        io.expect_print("This is your board now:")
-        io.expect_print("\n".join([
+        
+        expected_interactions = [
+            ("Welcome to the game!", None),
+            ("Set up your ships first.", None),
+            ("You have these ships remaining: 2, 3, 3, 4, 5", None),
+            ("Which do you wish to place?", "2"),
+            ("Vertical or horizontal? [vh]", "v"),
+            ("Which row?", "5"),
+            ("Which column?", "1"),
+            ("OK.", None),
+            ("This is your board now:", None),
+            ("\n".join([
             "..........",
             "..........",
-            ".S........",
-            ".S........",
             "..........",
             "..........",
+            "S.........",
+            "S.........",
             "..........",
             "..........",
             "..........",
             ".........."
-        ]))
-        io.expect_print("Done, for now!")
-        interface.run()
+            ]), None),
+            ("You have these ships remaining: 3, 3, 4, 5", None),
+            ("Which do you wish to place?", "3"),
+            ("Vertical or horizontal? [vh]", "v"),
+            ("Which row?", "5"),
+            ("Which column?", "2"),
+            ("OK.", None),
+            ("This is your board now:", None),
+            ("\n".join([
+            "..........",
+            "..........",
+            "..........",
+            "..........",
+            "SS........",
+            "SS........",
+            ".S........",
+            "..........",
+            "..........",
+            ".........."
+            ]), None),
+            ("You have these ships remaining: 3, 4, 5", None),
+            ("Which do you wish to place?", "3"),
+            ("Vertical or horizontal? [vh]", "h"),
+            ("Which row?", "1"),
+            ("Which column?", "1"),
+            ("OK.", None),
+            ("This is your board now:", None),
+            ("\n".join([
+            "SSS.......",
+            "..........",
+            "..........",
+            "..........",
+            "SS........",
+            "SS........",
+            ".S........",
+            "..........",
+            "..........",
+            ".........."
+            ]), None),
+            ("You have these ships remaining: 4, 5", None),
+            ("Which do you wish to place?", "4"),
+            ("Vertical or horizontal? [vh]", "v"),
+            ("Which row?", "5"),
+            ("Which column?", "3"),
+            ("OK.", None),
+            ("This is your board now:", None),
+            ("\n".join([
+            "SSS.......",
+            "..........",
+            "..........",
+            "..........",
+            "SSS.......",
+            "SSS.......",
+            ".SS.......",
+            "..S.......",
+            "..........",
+            ".........."
+            ]), None),
+            ("You have these ships remaining: 5", None),
+            ("Which do you wish to place?", "5"),
+            ("Vertical or horizontal? [vh]", "h"),
+            ("Which row?", "2"),
+            ("Which column?", "2"),
+            ("OK.", None),
+            ("This is your board now:", None),
+            ("\n".join([
+            "SSS.......",
+            ".SSSSS....",
+            "..........",
+            "..........",
+            "SSS.......",
+            "SSS.......",
+            ".SS.......",
+            "..S.......",
+            "..........",
+            ".........."
+            ]), None),
+        ]
+        
+        for interaction, user_input in expected_interactions:
+            io.expect_print(interaction)
+            if user_input:
+                io.provide(user_input)
+        
+        interface.run()  
