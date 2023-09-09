7ad84acd69b26ed59f2ac81c269b6e78f9a054ff
awdem <wdemontmollin@gmail.com>
2023-09-09 09:19:19 +0100

    refactor: removes behavior from game class that was transfered to new classes. Adds has_unplaced_ships method

--- lib/game.py
+++ lib/game.py
@@ -1,34 +1,12 @@
-from lib.ship import Ship
-from lib.board import Board
-from lib.ship_placement import ShipPlacement
-
+from lib.player import Player
 
 class Game:
     def __init__(self):
-        self.ships_placed = []
-        self.unplaced_ships =  [
-            Ship(2),
-            Ship(3),
-            Ship(3),
-            Ship(4),
-            Ship(5),
-        ]
-        self.board = Board()
+        self.player = Player()
 
-    def place_ship(self, length, orientation, row, col):
-        ship_placement = ShipPlacement(
-            length=length,
-            orientation=orientation,
-            row=row,
-            col=col,
-        )
-        self.ships_placed.append(ship_placement)
-        for ship in self.unplaced_ships:
-            if ship.length == length:
-                self.unplaced_ships.remove(ship)
-
-    def ship_at(self, row, col):
-        for ship_placement in self.ships_placed:
-            if ship_placement.covers(row, col):
+    def has_unplaced_ships(self):
+        for ship in self.player.board.ships:
+            if ship.orientation == None:
                 return True
-        return False
+            else:
+                return False
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,53 +1,36 @@
 from lib.game import Game
 
 """
-Initialises with a game board
+Initialises with a player
 """
-def test_initialises_with_a_length_and_width_of_10():
-    pass
+def test_initialises_with_a_player():
+    game = Game()
+    assert hasattr(game, 'player')
+    
+"""
+When the player has unplaced ships
+Then it returns true
+"""
+
+def test_when_player_has_unplaced_ships_it_returns_true():
+    game = Game()
+    assert game.has_unplaced_ships() == True
+
 
 """
-Initialises with five ships of length 2, 3, 3, 4, 5
+When the player has no unplaced ships
+Then it returns false
 """
-def test_initialises_with_five_ships_of_right_length():
-    game = Game()
-    unplaced_ships = game.unplaced_ships
-    assert len(unplaced_ships) == 5
-    assert unplaced_ships[0].length == 2
-    assert unplaced_ships[1].length == 3
-    assert unplaced_ships[2].length == 3
-    assert unplaced_ships[3].length == 4
-    assert unplaced_ships[4].length == 5
 
-"""
-Initialises with a totally empty board
-"""
-def test_initialises_with_a_totally_empty_board():
+def test_when_player_has_no_unplaced_ships_it_returns_false():
     game = Game()
-    for row in range(1, 11):
-        for col in range(1, 11):
-            assert not game.ship_at(row, col)
+    player = game.player
+    position = 0
 
-"""
-When we place a ship
-Then its place on the board is marked out
-"""
-def test_when_we_place_a_ship_then_its_place_on_the_board_is_marked_out():
-    game = Game()
-    game.place_ship(length=2, orientation="vertical", row=3, col=2)
-    assert game.ship_at(3, 2)
-    assert game.ship_at(4, 2)
-    assert not game.ship_at(3, 3)
-    assert not game.ship_at(4, 3)
-    assert not game.ship_at(3, 1)
-    assert not game.ship_at(4, 1)
-
-"""
-When we place a ship
-Then it is removed from the unplaced_ships array
-"""
-def test_when_we_place_a_ship_then_its_removed_from_the_unplaced_ships_array():
-    game = Game()
-    game.place_ship(length=2, orientation="vertical", row=3, col=2)
-    unplaced_ships = game.unplaced_ships
-    assert len(unplaced_ships) == 4
+    for ship in player.board.ships:
+        position += 1
+        ship.orientation = 'vertical'
+        ship.row = position
+        ship.col = position
+    
+    assert game.has_unplaced_ships() == False
