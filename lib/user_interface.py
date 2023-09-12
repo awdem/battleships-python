52dfc84136bf3a362f0bd3005831d5fb63484d7b
awdem <wdemontmollin@gmail.com>
2023-09-12 19:45:12 +0100

    refactor: replaces format_board with instance of BoardFormatter, separates welcome messages to a private method, corrects ship placeent loop

--- lib/user_interface.py
+++ lib/user_interface.py
@@ -1,27 +1,33 @@
+from lib.board_formatter import BoardFormatter
+
 class UserInterface:
     def __init__(self, io, game):
         self.io = io
         self.game = game
+        self.formatter = BoardFormatter()
 
     def run(self):
-        self._show("Welcome to the game!")
-        self._show("Set up your ships first.")
-        while len(self.game.unplaced_ships) > 0:
+        self._welcome()
+        while self.game.has_unplaced_ships():
             self._show("You have these ships remaining: {}".format(
                 self._ships_unplaced_message()))
             self._prompt_for_ship_placement()
             self._show("This is your board now:")
-            self._show(self._format_board())
+            self._show(self.formatter.format(self.game.player.board))
 
     def _show(self, message):
         self.io.write(message + "\n")
+        
+    def _welcome(self):
+        self._show("Welcome to the game!")
+        self._show("Set up your ships first.")   
 
     def _prompt(self, message):
         self.io.write(message + "\n")
         return self.io.readline().strip()
 
     def _ships_unplaced_message(self):
-        ship_lengths = [str(ship.length) for ship in self.game.unplaced_ships]
+        ship_lengths = [str(ship.length) for ship in self.game.player.board.unplaced_ships()]
         return ", ".join(ship_lengths)
 
     def _prompt_for_ship_placement(self):
@@ -30,21 +36,10 @@ class UserInterface:
         ship_row = self._prompt("Which row?")
         ship_col = self._prompt("Which column?")
         self._show("OK.")
-        self.game.place_ship(
+        self.game.player.board.place_ship(
             length=int(ship_length),
             orientation={"v": "vertical", "h": "horizontal"}[ship_orientation],
             row=int(ship_row),
             col=int(ship_col),
         )
 
-    def _format_board(self):
-        rows = []
-        for row in range(1, self.game.board.rows + 1):
-            row_cells = []
-            for col in range(1, self.game.board.cols + 1):
-                if self.game.ship_at(row, col):
-                    row_cells.append("S")
-                else:
-                    row_cells.append(".")
-            rows.append("".join(row_cells))
-        return "\n".join(rows)
